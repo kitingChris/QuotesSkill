2db66aa3f56c20b883cf6960956b532786e5707c
Christian Ego <Christian.Ego@c-ego.net>
2017-11-13 21:50:45 +0100

    Finished WikiQuotes Client

--- src/Client/WikiQuotes.js
+++ src/Client/WikiQuotes.js
@@ -1,36 +1,47 @@
 "use strict";
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const Querystring = require('querystring');
-const { JSDOM } = require("jsdom");
+const {JSDOM} = require("jsdom");
 
 class WikiQuotes {
     constructor(apiEndpoint, options) {
         this.apiEndpoint = apiEndpoint || 'https://en.wikiquote.org/w/api.php';
 
         this.options = Object.assign({
-            sectionsRegexFilter: "^[1](\\.?[0-9])*$"
+            quotesSection: "1",
+            quotesQuerySelector: "div > ul > li",
         }, options || {});
     }
 
     getRandomQuote(author, successHandler, errorHandler) {
+
+        errorHandler = errorHandler || _handleError;
+        successHandler = successHandler || console.log;
+
         const self = this;
 
 
-        const selectRandomQuote = function() {
-            const randomQuoteNum = Math.floor(Math.random()*quotes.quotes.length);
-            successHandler({ titles: quotes.titles, quote: quotes.quotes[randomQuoteNum] });
+        const selectRandomQuote = function (quotes) {
+            const randomQuoteNum = Math.floor(Math.random() * quotes.quotes.length);
+            successHandler({author: quotes.title, text: quotes.quotes[randomQuoteNum]});
         };
 
-        const getQuotesFromRandomSection = function(pageId, sections) {
-            const randomSectionNum = Math.floor(Math.random()*sections.sections.length);
-            self.getQuotesForSection(pageId, sections.sections[randomSectionNum], selectRandomQuote, errorHandler);
+        const getQuotesFromRandomSection = function (pageId, sections) {
+            const randomSectionNum = Math.floor(Math.random() * sections.sections.length);
+            self.getQuotesForSection(
+                pageId,
+                sections.sections[randomSectionNum],
+                self.options.quotesQuerySelector,
+                selectRandomQuote,
+                errorHandler
+            );
         };
 
-        const getSectionsForPage = function(pageid) {
+        const getSectionsForPage = function (pageid) {
             self.getSections(
                 pageid,
-                self.options.sectionsRegexFilter,
-                function(sections) {
+                self.options.quotesSection,
+                function (sections) {
                     getQuotesFromRandomSection(pageid, sections);
                 },
                 errorHandler);
@@ -44,7 +55,7 @@ class WikiQuotes {
     };
 
 
-    getQuotesForSection(pageid, sectionIndex, successHandler, errorHandler) {
+    getQuotesForSection(pageid, sectionIndex, quotesQuerySelector, successHandler, errorHandler) {
         _sendXMLHttpRequest({
             url: this.apiEndpoint,
             data: {
@@ -55,42 +66,34 @@ class WikiQuotes {
                 section: sectionIndex,
             },
             successHandler: function (responseText) {
-                let sections = [];
+
+                let quotes = [];
                 let title;
                 try {
                     const result = JSON.parse(responseText);
-                    const quotes = result.parse.text["*"];
 
-                    console.log(quotes);
+                    title = result.parse.title;
 
-                    //const dom = new JSDOM(quotes);
-                    //console.log(dom.window.document.querySelector("li").textContent);
+                    const quotesHtml = result.parse.text["*"];
 
+                    const dom = new JSDOM(quotesHtml);
 
+                    const quotesHtmlList = dom.window.document.querySelectorAll(quotesQuerySelector);
 
-                    /*const dom = (new DomParser()).parseFromString(quotes);
-
-                    let $li = dom.getElementsByTagName('li');
-
-                    for(let i in $li) {
-                        if($li.hasOwnProperty(i)) {
-                            console.log($li[i].innerHTML);
-                        }
-                    }*/
-
-
-
+                    quotesHtmlList.forEach(function (quotesHtmlListItem) {
+                        quotes.push(quotesHtmlListItem.textContent);
+                    });
 
                 } catch (e) {
                     _handleError(e);
                 }
-                /*
-                                if(sections.length === 0) {
-                                    _handleError('No quotes found on page '+(title||'with id: '+pageid));
-                                    return;
-                                }
 
-                                //successHandler({ title: title, sections: sections });*/
+                if (quotes.length === 0) {
+                    _handleError('No quotes found in section ' + sectionIndex + ' on page ' + (title || 'with id: ' + pageid));
+                    return;
+                }
+
+                successHandler({title: title, quotes: quotes});
             },
             errorHandler: errorHandler,
         });
@@ -106,15 +109,15 @@ class WikiQuotes {
                 redirects: "",
                 titles: titles,
             },
-            successHandler: function(responseText) {
+            successHandler: function (responseText) {
 
                 let pageid = null;
 
                 try {
                     const result = JSON.parse(responseText);
                     for (let p in result.query.pages) {
-                        if(result.query.pages.hasOwnProperty(p)) {
-                            if(!result.query.pages[p].hasOwnProperty('missing')) {
+                        if (result.query.pages.hasOwnProperty(p)) {
+                            if (!result.query.pages[p].hasOwnProperty('missing')) {
                                 pageid = result.query.pages[Object.keys(result.query.pages)[0]].pageid;
                                 break;
                             }
@@ -124,8 +127,8 @@ class WikiQuotes {
                     _handleError(e);
                 }
 
-                if(pageid === null) {
-                    _handleError('No quotes found for '+titles);
+                if (pageid === null) {
+                    _handleError('No quotes found for ' + titles);
                     return;
                 }
 
@@ -135,7 +138,7 @@ class WikiQuotes {
         });
     };
 
-    getSections(pageid, sectionsFilter, successHandler, errorHandler) {
+    getSections(pageid, quotesSection, successHandler, errorHandler) {
         _sendXMLHttpRequest({
             url: this.apiEndpoint,
             data: {
@@ -149,28 +152,38 @@ class WikiQuotes {
                 let title;
                 try {
                     const result = JSON.parse(responseText);
+                    //console.log(responseText);
                     title = result.parse.title;
-                    for(let s in result.parse.sections) {
-                        if(result.parse.sections.hasOwnProperty(s)) {
-                            if(typeof sectionsFilter !== "undefined") {
-                                const regExp = new RegExp(sectionsFilter);
-                                if(regExp.exec(result.parse.sections[s].number) === null) {
-                                    continue
-                                }
+
+                    let primarySectionIndex;
+
+                    // Getting only subsections that matches the configured quotes section
+                    for (let s in result.parse.sections) {
+                        if (result.parse.sections.hasOwnProperty(s)) {
+                            let splitNum = result.parse.sections[s].number.split('.');
+                            if (splitNum.length > 1 && splitNum[0] === quotesSection) {
+                                sections.push(result.parse.sections[s].index);
+                            } else if (splitNum.length === 1 && splitNum[0] === quotesSection) {
+                                primarySectionIndex = result.parse.sections[s].index;
                             }
-                            sections.push(result.parse.sections[s].index);
                         }
                     }
+
+                    // If the main section has no subsection we use the main section instead
+                    if (sections.length === 0) {
+                        sections.push(primarySectionIndex);
+                    }
+
                 } catch (e) {
                     _handleError(e);
                 }
 
-                if(sections.length === 0) {
-                    _handleError('No quotes found on page '+(title||'with id: '+pageid));
+                if (sections.length === 0) {
+                    _handleError('No quotes found on page ' + (title || 'with id: ' + pageid));
                     return;
                 }
 
-                successHandler({ title: title, sections: sections });
+                successHandler({title: title, sections: sections});
             },
             errorHandler: errorHandler,
         });
@@ -187,7 +200,7 @@ function _sendXMLHttpRequest(options) {
     options.withCredentials = options.withCredentials || true;
     options.data = options.data || {};
 
-    if(options.method === "GET" && Object.keys(options.data).length > 0) {
+    if (options.method === "GET" && Object.keys(options.data).length > 0) {
         options.url = options.url + '?' + Querystring.stringify(options.data);
     }
 
@@ -198,7 +211,7 @@ function _sendXMLHttpRequest(options) {
             if (this.status >= 200 && this.status < 300) {
                 options.successHandler(this.responseText);
             } else {
-                options.errorHandler(this.responseText || this.statusText || 'Error: '+this.status);
+                options.errorHandler(this.responseText || this.statusText || 'Error: ' + this.status);
             }
         }
     });
--- src/wikiquotes.js
+++ src/wikiquotes.js
@@ -1,10 +1,16 @@
 const WikiQuotes = require('./Client/WikiQuotes');
 
 
-const wq1 = new WikiQuotes('https://en.wikiquote.org/w/api.php', {sectionsRegexFilter:"^1(\\.[0-9])*$"});
+const wq1 = new WikiQuotes('https://en.wikiquote.org/w/api.php', {quotesSection:"1", querySelectorAll: "div > ul > li"});
 //wq1.getRandomQuote('Albert');
-wq1.getRandomQuote('Albert Einstein');
+wq1.getRandomQuote('Albert Einstein', function (quote) {
+    console.log("-".repeat(40))
+    console.log(quote.text);
+    console.log("\t\t- ", quote.author);
+    console.log("-".repeat(40))
+});
 
-const wq2 = new WikiQuotes('https://de.wikiquote.org/w/api.php', {sectionsRegexFilter:"^2(\\.[0-9])*$"});
+const wq2 = new WikiQuotes('https://de.wikiquote.org/w/api.php', {quotesSection:"2", querySelectorAll: "div > ul > li"});
 //wq2.getRandomQuote('Albert');
+
 wq2.getRandomQuote('Albert Einstein');
